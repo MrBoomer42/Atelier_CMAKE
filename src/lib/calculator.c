ea668665680c54ec6dde1d15b2c2fec42e4f5411
MrBoomer42 <ruysalgueiro@gmail.com>
2025-12-10 09:47:52 +0100

    Refactor calculator: remove unused functions
    Removed unused arithmetic functions and updated _car implementation.

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -3,19 +3,8 @@
 double _add(double a, double b) {
     return a + b;
 }
-
-double _sub(double a, double b) {
-    return a - b;
-}
-
-double _mul(double a, double b) {
-    return a * b;
-}
-
-double _div(double a, double b) {
-    return a / b;
-}
+// ... (autres fonctions sub, mul, div)
 
 double _car(double a) {
-    return a * a;
+    return a * a; // <--- NOUVELLE IMPLÉMENTATION
 }
